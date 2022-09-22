e7f8628bf4f3e51ff6e9cb47a0e9e5dca75200e8
GurudeepNaik <gurudeepnaik1999@gmail.com>
2022-09-22 11:28:02 +0530

    Changes made

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const route = require("./routes/route.js");
@@ -34,26 +33,4 @@ app.get("*", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server is up at http://localhost:${PORT}/`);
-=======
-require("dotenv").config();
-const port =  8080;
-const mongoose = require("mongoose");
-const app = express();
-const login = require("./routers/login");
-const register = require("./routers/register")
-const bodyParser=require('body-parser')
-
-mongoose.connect("mongodb+srv://contactmanager:contactmanager@cluster0.jvlvlig.mongodb.net/Contact-Manager?retryWrites=true&w=majority",
-{ useNewUrlParser: true, useUnifiedTopology: true },()=>console.log("DB is connected"))
-
-
-app.use(bodyParser())
-app.use("/login",login);
-app.use("/register",register)
-
-
-
-app.listen(port, () => {
-  console.log(`Server is up at http://localhost:${port}/`);
->>>>>>> df902e338dccba6496926d21ad3dd6ad87f0c971
 });
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
@@ -9,16 +8,3 @@ const UserSchema = mongoose.Schema({
 const User = mongoose.model("users", UserSchema);
 
 module.exports = User;
-=======
-const mongoose = require ("mongoose");
-
-const UserSchema = mongoose.Schema({
-    email:{type:String , require:true},
-    password:{type:String , require:true}
-
-})
-
-const User = mongoose.model("users", UserSchema)
-
-module.exports = User
->>>>>>> df902e338dccba6496926d21ad3dd6ad87f0c971
