a7496037eae602a101a7c64ac2b0e544fd14d732
GurudeepNaik <gurudeepnaik1999@gmail.com>
2022-09-24 19:40:42 +0530

    Added all Files again

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 require("dotenv").config();
 
 mongoose.connect(
